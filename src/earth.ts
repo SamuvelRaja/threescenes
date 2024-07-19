505dc860aef1efd73aaffc76fa8e3950f7523f2b
SamuvelRaja <josephsamuvel001@gmail.com>
2024-07-19 20:29:01 +0530

    added rendereer tracking

--- src/earth.ts
+++ src/earth.ts
@@ -1,89 +1,91 @@
-import * as THREE from "three"
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 import { mainOptions } from "./helper/option";
 import getStarfield from "./helper/stars";
 import resize from "./helper/resize";
-
+import { rendercp } from "./main";
 //reusing renderer options
-const { w, h, fov, ratio, near} = mainOptions;
+const { w, h, fov, ratio, near } = mainOptions;
 
 //adding renderer
-const earthRenderer=new THREE.WebGLRenderer({antialias:true})
-earthRenderer.setSize(w,h)
+const earth = () => {
+  const earthRenderer = new THREE.WebGLRenderer({ antialias: true });
+  rendercp=earthRenderer
+  earthRenderer.setSize(w, h);
 
-document.body.appendChild(earthRenderer.domElement)
+  document.body.appendChild(earthRenderer.domElement);
 
+  earthRenderer.toneMapping = THREE.ACESFilmicToneMapping;
+  earthRenderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 
+  //adding camera
+  const earthCamera = new THREE.PerspectiveCamera(fov, ratio, near, 1000);
+  earthCamera.position.z = 3;
+  const earthScene = new THREE.Scene();
 
-earthRenderer.toneMapping = THREE.ACESFilmicToneMapping;
-earthRenderer.outputColorSpace = THREE.LinearSRGBColorSpace;
+  //orbit controls for interaction
+  const controls = new OrbitControls(earthCamera, earthRenderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 1.0;
+  controls.enableZoom = false;
 
-//adding camera
-const earthCamera=new THREE.PerspectiveCamera(fov,ratio,near,1000)
-earthCamera.position.z=3
-const earthScene=new THREE.Scene()
+  //earth group creation
+  const earthGroup = new THREE.Group();
+  earthScene.add(earthGroup);
 
-//orbit controls for interaction
-const controls=new OrbitControls(earthCamera,earthRenderer.domElement);
-controls.enableDamping=true;
-controls.dampingFactor=1.0
-controls.enableZoom=false
-
-//earth group creation
-const earthGroup=new THREE.Group()
-earthScene.add(earthGroup)
-
-//adding basic earth mesh(geometry+material)
-const loader=new THREE.TextureLoader
-const earthGeo=new THREE.IcosahedronGeometry(1,12)
-const earthMat=new THREE.MeshPhongMaterial({
-    map:loader.load("/textures/00_earthmap1k.jpg"),
+  //adding basic earth mesh(geometry+material)
+  const loader = new THREE.TextureLoader();
+  const earthGeo = new THREE.IcosahedronGeometry(1, 12);
+  const earthMat = new THREE.MeshPhongMaterial({
+    map: loader.load("/textures/00_earthmap1k.jpg"),
     specularMap: loader.load("/textures/02_earthspec1k.jpg"),
     bumpMap: loader.load("/textures/01_earthbump1k.jpg"),
     bumpScale: 0.04,
-})
-const earthMesh=new THREE.Mesh(earthGeo,earthMat)
-earthGroup.add(earthMesh)
-earthGroup.rotation.z = -23.4 * Math.PI / 180;
-//adding night lights
-const earthlight=new THREE.MeshBasicMaterial({
-    map:loader.load("/textures/03_earthlights1k.jpg"),
-    blending:THREE.AdditiveBlending
-})
-const lightMesh=new THREE.Mesh(earthGeo,earthlight)
-earthGroup.add(lightMesh)
+  });
+  const earthMesh = new THREE.Mesh(earthGeo, earthMat);
+  earthGroup.add(earthMesh);
+  earthGroup.rotation.z = (-23.4 * Math.PI) / 180;
+  //adding night lights
+  const earthlight = new THREE.MeshBasicMaterial({
+    map: loader.load("/textures/03_earthlights1k.jpg"),
+    blending: THREE.AdditiveBlending,
+  });
+  const lightMesh = new THREE.Mesh(earthGeo, earthlight);
+  earthGroup.add(lightMesh);
 
-//adding earth cloud
-const earthCloud=new THREE.MeshStandardMaterial({
-    map:loader.load("/textures/04_earthcloudmap.jpg"),
+  //adding earth cloud
+  const earthCloud = new THREE.MeshStandardMaterial({
+    map: loader.load("/textures/04_earthcloudmap.jpg"),
     transparent: true,
     opacity: 0.8,
     blending: THREE.AdditiveBlending,
-    alphaMap: loader.load('./textures/05_earthcloudmaptrans.jpg'),
-    })
-const cloudMesh=new THREE.Mesh(earthGeo,earthCloud)
-cloudMesh.scale.setScalar(1.003);
-earthGroup.add(cloudMesh)
+    alphaMap: loader.load("./textures/05_earthcloudmaptrans.jpg"),
+  });
+  const cloudMesh = new THREE.Mesh(earthGeo, earthCloud);
+  cloudMesh.scale.setScalar(1.003);
+  earthGroup.add(cloudMesh);
 
-const stars = getStarfield({numStars: 3000});
+  const stars = getStarfield({ numStars: 3000 });
 
-earthScene.add(stars);
+  earthScene.add(stars);
 
-//adding sunlight
-const sunlight=new THREE.DirectionalLight(0xffffff,2)
-sunlight.position.set(-2,0.5,1.5)
-earthGroup.add(sunlight)
+  //adding sunlight
+  const sunlight = new THREE.DirectionalLight(0xffffff, 2);
+  sunlight.position.set(-2, 0.5, 1.5);
+  earthGroup.add(sunlight);
 
-function earthAnimate(){
-    requestAnimationFrame(earthAnimate)
-    earthMesh.rotation.y+=0.003
-    lightMesh.rotation.y+=0.003
-    cloudMesh.rotation.y+=0.003
-     stars.rotation.y -= 0.0002;
-    earthRenderer.render(earthScene,earthCamera)
-        controls.update()    
-}
-earthAnimate()
+  function earthAnimate() {
+    requestAnimationFrame(earthAnimate);
+    earthMesh.rotation.y += 0.003;
+    lightMesh.rotation.y += 0.003;
+    cloudMesh.rotation.y += 0.003;
+    stars.rotation.y -= 0.0002;
+    earthRenderer.render(earthScene, earthCamera);
+    controls.update();
+  }
+  earthAnimate();
 
-resize(earthCamera,earthRenderer)
+  resize(earthCamera, earthRenderer);
+};
 
+export default earth;
--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,59 @@
-
-import * as THREE from "three"
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 import resize from "./helper/resize";
 
-const w:number=window.innerWidth;
-const h:number=window.innerHeight;
+const w: number = window.innerWidth;
+const h: number = window.innerHeight;
 
-const ratio:number=w/h;
-const fov:number=75;
-const near:number=0.1;
-const far:number=10;
+const ratio: number = w / h;
+const fov: number = 75;
+const near: number = 0.1;
+const far: number = 10;
+export let rendercp:THREE.WebGLRenderer;
 
+const orb = () => {
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  rendercp=renderer
+  renderer.setSize(w, h);
 
-const renderer=new THREE.WebGLRenderer({antialias:true})
+  document.querySelector("#app")?.appendChild(renderer.domElement);
 
-renderer.setSize(w,h);
+  const camera = new THREE.PerspectiveCamera(fov, ratio, near, far);
+  camera.position.z = 3;
+  const scene = new THREE.Scene();
+  const geo = new THREE.IcosahedronGeometry(1.0, 5);
 
-document.querySelector("#app")?.appendChild(renderer.domElement)
+  //The material used for the mesh.
 
+  const mat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    flatShading: true,
+  });
+  const light = new THREE.HemisphereLight(0x002288, 0x99cb00);
+  scene.add(light);
+  const mesh = new THREE.Mesh(geo, mat); //geometry + material creates a mesh
+  scene.add(mesh); //then mesh added to the scene
 
-const camera=new THREE.PerspectiveCamera(fov,ratio,near,far)
-camera.position.z=3
-const scene=new THREE.Scene()
-const geo=new THREE.IcosahedronGeometry(1.0,5)
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.03;
+  controls.enableZoom = false;
+  //wire mesh
+  const wire = new THREE.MeshBasicMaterial({
+    color: 0x990000,
+    wireframe: true,
+  });
+  const wiremesh = new THREE.Mesh(geo, wire);
+  mesh.add(wiremesh);
 
- //The material used for the mesh.
+  //animate scene
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    controls.update();
+  }
+  animate();
 
-const mat=new THREE.MeshStandardMaterial({
-  color:0xffffff,
-  flatShading:true
-})
-const light=new THREE.HemisphereLight(0x002288,0x99cb00)
-scene.add(light)
-const mesh=new THREE.Mesh(geo,mat)//geometry + material creates a mesh 
-scene.add(mesh)//then mesh added to the scene
-
-const controls=new OrbitControls(camera,renderer.domElement)
-controls.enableDamping=true
-controls.dampingFactor=0.03
-controls.enableZoom=false
-//wire mesh
-const wire=new THREE.MeshBasicMaterial({
-  color:0x990000,
-  wireframe:true
-})
-const wiremesh =new THREE.Mesh(geo,wire)
-mesh.add(wiremesh)
-
-
-//animate scene
-function animate(){
-  requestAnimationFrame(animate)
-  renderer.render(scene,camera)
-  controls.update()
-}
-animate()
-
-resize(camera,renderer)
+  resize(camera, renderer);
+};
+export default orb;
